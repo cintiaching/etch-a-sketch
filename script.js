a23f3670f287ad85905b502f6dd5c5e77051be56
cintiaching <swching.cintia@gmail.com>
2025-09-10 21:00:38 +0800

    create the grid

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const container = document.querySelector("div.container");
 gridSize = 16
 for (let i = 0; i < gridSize*gridSize; i++) {
     const div = document.createElement("div")
-    div.textContent = 1
     div.classList.add("grid");
     container.appendChild(div)
 }
