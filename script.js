c4bd804e0b176248817d61ebb0ec2d94dec4d2e5
cintiaching <swching.cintia@gmail.com>
2025-09-10 21:11:31 +0800

    “hover” effect

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 
 const container = document.querySelector("div.container");
 
+// create grid
 gridSize = 16
 for (let i = 0; i < gridSize*gridSize; i++) {
     const div = document.createElement("div")
     div.classList.add("grid");
     container.appendChild(div)
 }
+
+// "hover" effect
+
+const grids = document.querySelectorAll("div.grid");
+
+grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+        console.log(grid)
+        grid.setAttribute("id", "colored")
+    })
+})
